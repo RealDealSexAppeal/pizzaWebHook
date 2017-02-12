4320c076373bdb0ec5c4de89722874ecc2ae8b9d
Luis Ortiz <luisortiz@Luiss-MacBook-Pro.local>
2017-02-12 03:03:36 -0800

    bootylickin

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = {
+        "speech": "Lick my booty",
+        "displayText": "Lick my booty",
+        # "data": data,
+        # "contextOut": [],
+        "source": "pizza-webhook-sample"
+    }
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -31,68 +37,68 @@ def webhook():
     return r
 
 
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.request.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+# def processRequest(req):
+#     if req.get("result").get("action") != "yahooWeatherForecast":
+#         return {}
+#     baseurl = "https://query.yahooapis.com/v1/public/yql?"
+#     yql_query = makeYqlQuery(req)
+#     if yql_query is None:
+#         return {}
+#     yql_url = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
+#     result = urllib.request.urlopen(yql_url).read()
+#     data = json.loads(result)
+#     res = makeWebhookResult(data)
+#     return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+# def makeYqlQuery(req):
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     city = parameters.get("geo-city")
+#     if city is None:
+#         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+#     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+# def makeWebhookResult(data):
+#     query = data.get('query')
+#     if query is None:
+#         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+#     result = query.get('results')
+#     if result is None:
+#         return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+#     channel = result.get('channel')
+#     if channel is None:
+#         return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+#     item = channel.get('item')
+#     location = channel.get('location')
+#     units = channel.get('units')
+#     if (location is None) or (item is None) or (units is None):
+#         return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+#     condition = item.get('condition')
+#     if condition is None:
+#         return {}
 
-    # print(json.dumps(item, indent=4))
+#     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+#     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+#              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    print("Response:")
-    print(speech)
+#     print("Response:")
+#     print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "pizza-webhook-sample"
-    }
+#     return {
+#         "speech": speech,
+#         "displayText": speech,
+#         # "data": data,
+#         # "contextOut": [],
+#         "source": "pizza-webhook-sample"
+#     }
 
 
 if __name__ == '__main__':
